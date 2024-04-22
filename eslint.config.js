294bae4f7b072afd06847a9d446c887e5ec12889
Ilma Ogrešević <107383087+Ilmaooo@users.noreply.github.com>
2024-04-22 14:14:47 +0200

    added eslint configuration for wdio (#2)
    * added eslint configuration for wdio
    
    * removed unnecessary package

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import {configs, rules} from "eslint-plugin-wdio";
+
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: configs.recommended.globals
+    },
+    plugins:{
+      wdio: { rules }
+    }, 
+    rules: {
+      "wdio/await-expect": "error",
+      "wdio/no-debug": "error",
+      "wdio/no-pause": "error",
+    },
+  },
+];
