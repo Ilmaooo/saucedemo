87dc03b4c1a1b2698fa65325068f8d3cacf06c67
Ilma Ogresevic <ilmaogresevic1@gmail.com>
2024-04-25 17:43:55 +0200

    added missing await

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -46,7 +46,7 @@ class HomePage extends Page {
 
     const prices = await priceElements.map(async (element) => element.getText())
 
-    const priceValues = prices.map((price) =>
+    const priceValues = await prices.map((price) =>
       parseFloat(price.replace(/[^0-9.-]+/g, '')),
     )
 
