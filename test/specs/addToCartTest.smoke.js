1dfbf1f006fae0ebf6f33fd45fbe45bf27c6e85f
Ilma Ogresevic <ilmaogresevic1@gmail.com>
2024-04-29 17:00:28 +0200

    get username argument from command line if present

--- test/specs/addToCartTest.smoke.js
+++ test/specs/addToCartTest.smoke.js
@@ -1,21 +1,28 @@
-import Page from '../pageobjects/page.js'
 import LoginPage from '../pageobjects/login.page.js'
 import HomePage from '../pageobjects/home.page.js'
 import ProductPage from '../pageobjects/product.page.js'
 import CartPage from '../pageobjects/cart.page.js'
 
-console.log('Command-line arguments:', process.argv) // Log process.argv at the beginning
-
 describe('Add to cart', () => {
   it('should add product to cart from product page', async () => {
-    const username = process.argv[6] //get username from command line
+    //get command line arguments starting from index 2
+    const args = process.argv.slice(2)
 
-    if (!username) {
-      throw new Error('Username not provided. Please specify the username')
-    }
+    // Find the argument containing '--username='
+    const usernameArg = args.find((arg) => arg.startsWith('--username='))
+
+    // Extract the username if the argument exists
+    const username = usernameArg
+      ? usernameArg.split('=')[1]
+      : process.env.USERNAME1 // Default to USERNAME1 if not specified
+    console.log('Command-line arguments:', args)
+    console.log('Username index:', usernameArg)
+    console.log('Username:', username)
+
     // Login and navigate to the home page
     await LoginPage.open()
     await LoginPage.login(username, process.env.PASSWORD)
+    console.log('username:', username)
     await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html')
 
     // Click on the product using the custom locator
